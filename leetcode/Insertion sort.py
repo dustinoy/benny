4e341b6e762efcc900c7b2d0d5de15cc70bc67a1
benny <42614064+Benny805038@users.noreply.github.com>
2019-10-06 18:54:54 +0800

    Update Insertion sort.py

--- leetcode/Insertion sort.py
+++ leetcode/Insertion sort.py
@@ -6,17 +6,20 @@
 
 class Solution:
      def insertionSortList(self, head):
-          #把數列的第一項指定是頭
+          # 把數列的第一項指定是頭
         newHead = ListNode(0)
         newHead.next = head 
         while head and head.next
-            #跳過已經排序好的   
+            # 跳過已經排序好的   
             if head.val > head.next.val:
                 nodeToInsert = head.next
+               # 開始比較
                 cur = newHead
                 while cur.next and cur.next.val<nodeToInsert.val:
                     cur = cur.next
+                    # 將基準值指定到下一個要找的值
                 head.next = nodeToInsert.next
+               # 插到比自己大的值之前
                 nodeToInsert.next = cur.next
                 cur.next = nodeToInsert
             else:
